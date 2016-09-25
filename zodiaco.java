798347d9e88d30ca2c82ca5aa2a0b84dcf4eabcb
Enith <yeseniaerr@gmail.com>
2016-09-25 14:41:11 -0700

    imprime signo zodiacal

--- zodiaco.java
+++ zodiaco.java
@@ -41,5 +41,92 @@ public class zodiaco {
 		año = sc.nextInt(); 
 
 
+		switch(mes) {		
+		  case 1: 
+		    // Enero
+		    if (dia>=20)
+		      System.out.println("Acuario");
+		    else
+		      System.out.println("Capricornio");				
+		    break;
+		  case 2:
+		    // Febrero
+		    if (dia>=19)
+		      System.out.println("Piscis");
+		    else
+		      System.out.println("Acuario");
+		    break;
+		case 3:
+		    // Marzo
+		    if (dia>=21)
+		      System.out.println("Aries");
+		    else
+		      System.out.println("Picis");
+		    break;
+		case 4:
+		    // Abril
+		    if (dia>=20)
+		      System.out.println("Tauro");
+		    else
+		      System.out.println("Aries");
+		    break;
+		 case 5:
+		    // Mayo
+		    if (dia>=21)
+		      System.out.println("Geminis");
+		    else
+		      System.out.println("Tauro");
+		    break;
+		case 6:
+		    // Junio
+		    if (dia>=21)
+		      System.out.println("Cancer");
+		    else
+		      System.out.println("Geminis");
+		    break;
+		 case 7:
+		    // Julio
+		    if (dia>=23)
+		      System.out.println("Leo");
+		    else
+		      System.out.println("Cancer");
+		    break;  
+		case 8:
+		    // Agosto
+		    if (dia>=23)
+		      System.out.println("Virgo");
+		    else
+		      System.out.println("Leo");
+		    break; 
+	        case 9:
+		    // Septiembre
+		    if (dia>=23)
+		      System.out.println("Libra");
+		    else
+		      System.out.println("Virgo");
+		    break; 
+		case 10:
+		    // Octubre
+		    if (dia>=23)
+		      System.out.println("Escorpion");
+		    else
+		      System.out.println("Libra");
+		    break;
+		case 11:
+		    // Noviembre
+		    if (dia>=22)
+		      System.out.println("Sagitario");
+		    else
+		      System.out.println("Escorpion");
+		    break;
+		case 12:
+		    // Diciembre
+		    if (dia>=22)
+		      System.out.println("Capricornio");
+		    else
+		      System.out.println("sagitario");
+		    break;
+		}
+	
 
 }}
